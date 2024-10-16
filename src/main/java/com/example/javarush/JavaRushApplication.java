2f61c953cffe34d7dc81603108ff2e81e25dddf0
Dias Rakhman <rahmanovdias13@gmail.com>
2024-10-16 17:37:01 +0500

    first commit

---
+++ src/main/java/com/example/javarush/JavaRushApplication.java
@@ -0,0 +1,13 @@
+package com.example.javarush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javarush/JavaRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
