07a3439fefcd1e4adbf3bb56a25b65650bdfb3ba
Dias Rakhman <rahmanovdias13@gmail.com>
2024-10-17 17:31:05 +0500

    The frontend part of the registration and authorization forms is completed and is ready for connection to Spring Security

--- src/main/java/com/example/javarush/Controller/UserController.java
+++ src/main/java/com/example/javarush/Controller/UserController.java
@@ -1,12 +1,20 @@
 package com.example.javarush.Controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "users/login";
+    }
+
 
 
 
---
+++ src/main/java/com/example/javarush/Controller/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.example.javarush.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping("")
+    public String welcome() {
+        return "welcome/welcome";
+    }
+
+    @GetMapping("/last-step")
+    public String register() {
+        return "users/register";
+    }
+
+    // @PostMapping("/last-step")
+
+
+}
