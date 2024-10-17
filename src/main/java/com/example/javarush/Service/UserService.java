ee3bf5040b88ec15229b0d5b494165837ffe3c24
Dias Rakhman <rahmanovdias13@gmail.com>
2024-10-17 12:32:43 +0500

    The database is connected and the first user table is created.

---
+++ src/main/java/com/example/javarush/Service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.javarush.Service;
+
+import com.example.javarush.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
